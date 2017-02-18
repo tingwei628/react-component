5f5455c8240ee84bdea6f6b68a85a22664cc96f7
陳廷維 <sbvq6p199xe@gmail.com>
2017-02-19 01:41:11 +0800

    add rollup.config.js

---
+++ rollup/rollup.config.js
@@ -0,0 +1,60 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import uglify from 'rollup-plugin-uglify';
+import replace from 'rollup-plugin-replace';
+import path from 'path';
+export default {
+  entry: path.resolve(
+    __dirname,
+    '../examples/HOC/hoc1.js'
+  ),
+  dest: path.resolve(
+    __dirname,
+    '../examples/HOC/build/hoc1.min.js'
+  ),
+  format: 'iife',
+  plugins: [
+    babel({
+      presets: [
+        'react',
+        ['es2015',{'modules': false}],
+	'stage-0'
+      ],
+      exclude: path.resolve(__dirname, '../node_modules/**'),
+      babelrc: false,
+      plugins: ['external-helpers'],
+      externalHelpers: true
+    }),
+    nodeResolve({
+      module: true,
+      jsnext: true,
+      main: true,
+      browser: true
+    }),
+    commonjs({
+      include: path.resolve(__dirname, '../node_modules/**'),
+      nameExports: {
+        [path.resolve(__dirname, '../node_modules/react/react.js')]: [
+	  'PropTypes',
+	  'Component'
+	]
+      }
+    }),
+    replace({
+      // this is development enviroment
+      // if you delploy on production, just change 'development' to 'production'
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    (process.env.NODE_ENV === 'production' && uglify({
+      compress: {
+        screw_ie8: true,
+        warning: 'false'
+      },
+      output: {
+        comments: false
+      },
+      sourceMap: false
+    }))
+  ]
+}
